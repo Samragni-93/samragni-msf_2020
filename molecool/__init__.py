4bb2435604baf310557740abfa0511c7ae48cbb0
Samragni Banerjee <samragnibanerjee@Samragnis-MacBook-Pro.local>
2020-02-04 10:49:27 -0500

    Added files

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -5,7 +5,7 @@ A python package for the MSF 2020 Bootcamp
 
 # Add imports here
 from .functions import *
-from .atomdata import atomic weights,atom_colors
+from .atomdata import atomic_weights,atom_colors
 from .measure import calculate_distance, calculate_angle
 from .molecule import build_bond_list
 from .visualize import bond_histogram, drwa_molecule
